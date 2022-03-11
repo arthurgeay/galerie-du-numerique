b11a22af9371105fd7bf2cdc378e6063549cd469
Arthur Geay <arthurgeay.contact@gmail.com>
2022-03-11 15:02:44 +0100

    chore: update ci github action

--- artwork_gallery/artwork_gallery/asgi.py
+++ artwork_gallery/artwork_gallery/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artwork_gallery.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artwork_gallery.settings")
 
 application = get_asgi_application()
