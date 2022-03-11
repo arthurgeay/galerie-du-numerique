40f366242f7d4bbcf924828e3023e3e58a1154d4
Arthur Geay <arthurgeay.contact@gmail.com>
2022-03-12 00:25:46 +0100

    feat(artwork): add artwork model

--- artwork_gallery/artworks/models.py
+++ artwork_gallery/artworks/models.py
@@ -16,3 +16,17 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+class Artwork(models.Model):
+    title = models.fields.CharField(max_length=255)
+    description = models.fields.TextField(null=True, blank=True)
+    image = models.fields.TextField(null=True, blank=True)
+    released_at = models.fields.CharField(max_length=4, null=True, blank=True)
+    width = models.fields.CharField(max_length=10, null=True, blank=True)
+    height = models.fields.CharField(max_length=10, null=True, blank=True)
+    location = models.fields.CharField(max_length=255, null=True, blank=True)
+    artist = models.ForeignKey(Artist, null=False, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
