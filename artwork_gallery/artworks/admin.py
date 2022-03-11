9312de1ed3863ae7b0ad2f952bf6c736bf3a598b
Arthur Geay <arthurgeay.contact@gmail.com>
2022-03-12 00:01:25 +0100

    feat(artworks): add artist model

--- artwork_gallery/artworks/admin.py
+++ artwork_gallery/artworks/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Category
+from .models import Artist
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birthday', 'deathday', 'image')
 
 admin.site.register(Category)
+admin.site.register(Artist, ArtistAdmin)
--- artwork_gallery/artworks/models.py
+++ artwork_gallery/artworks/models.py
@@ -6,3 +6,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Artist(models.Model):
+    name = models.fields.CharField(max_length=255)
+    birthday = models.fields.CharField(max_length=20, null=True, blank=True)
+    deathday = models.fields.CharField(max_length=20, null=True, blank=True)
+    image = models.fields.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
