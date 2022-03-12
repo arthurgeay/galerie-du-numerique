0d688ceebf9bc4960d6b85cd380857dbf7d91050
Arthur Geay <arthurgeay.contact@gmail.com>
2022-03-12 01:03:41 +0100

    feat(artworks): CRUD for artwork, artist and category (#29)
    * feat(artworks): create category model
    
    * feat(artworks): add artist model
    
    * feat(artwork): add artwork model
    
    * feat(artworks): update models with unique constraint

---
+++ artwork_gallery/artworks/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Category
+from .models import Artist
+from .models import Artwork
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("name", "birthday", "deathday", "image")
+
+
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = ("title", "artist", "category")
+
+
+admin.site.register(Category)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Artwork, ArtworkAdmin)
---
+++ artwork_gallery/artworks/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.fields.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    name = models.fields.CharField(max_length=255, unique=True)
+    birthday = models.fields.CharField(max_length=20, null=True, blank=True)
+    deathday = models.fields.CharField(max_length=20, null=True, blank=True)
+    image = models.fields.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Artwork(models.Model):
+    title = models.fields.CharField(max_length=255, unique=True)
+    description = models.fields.TextField(null=True, blank=True)
+    image = models.fields.TextField(null=True, blank=True)
+    released_at = models.fields.CharField(max_length=4, null=True, blank=True)
+    width = models.fields.CharField(max_length=10, null=True, blank=True)
+    height = models.fields.CharField(max_length=10, null=True, blank=True)
+    location = models.fields.CharField(max_length=255, null=True, blank=True)
+    artist = models.ForeignKey(Artist, null=False, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
