c65788a708be9e6d2b5837a1b4f47340eb25098c
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-03-17 09:53:17 +0100

    ☄ adding views and template for gallery

--- artwork_gallery/artwork_gallery/urls.py
+++ artwork_gallery/artwork_gallery/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from artwork_gallery import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("authentication.urls")),
+    path("gallery", views.home, name="home"),
 ]
