b2720dbe9d6aad1f806d08625855eadb443c05e6
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-03-17 10:57:27 +0100

    🚧 adding script tag dynamically

--- artwork_gallery/artwork_gallery/views.py
+++
@@ -1,7 +0,0 @@
-from django.template import loader
-from django.http import HttpResponse
-
-def home(request):
-    template = loader.get_template('artwork_gallery/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request), status=201)
