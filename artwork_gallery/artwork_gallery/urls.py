d652a315596ab69c89fc7af87a9cd59d2909586d
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-03-19 01:42:14 +0100

    Intégration de la page de Presentation et des pages légales (#46)
    * ☄ adding views and template for gallery
    
    * 🚧 base for styling
    
    * 🚚 moving files into artworks app
    
    * 🚧 adding script tag dynamically
    
    * 🎏 styling header
    
    * 🚧 creating structure for gallery
    
    * 🎏 styling card
    
    * 🎏 styling cards
    
    * 🩹 fix hover and focus a color
    
    * 🎏 creating presentation place component
    
    * 🚧 adding footer
    
    * 🚧 creating presentation page
    
    * 🚧 addding policy page
    
    * 🚧 adding infos
    
    * 💬 adding user agreement
    
    * 🚧 hero on presentation
    
    * 🚧 adding testimonials
    
    * 🚧 adding testimonials
    
    * 🚧 adding squares
    
    * 🎏 align footer
    
    * 🎏 add color on signin button
    
    * fix: login and signup
    
    * style: alert info
    
    Co-authored-by: Arthur Geay <arthurgeay.contact@gmail.com>

--- artwork_gallery/artwork_gallery/urls.py
+++ artwork_gallery/artwork_gallery/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("authentication.urls")),
+    path("", include("presentation.urls")),
     path("artworks/", include("artworks.urls")),
     path("polls/", include("polls.urls")),
 ]
