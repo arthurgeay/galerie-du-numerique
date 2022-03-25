582eb815dc9f99eaa9b9118af963777bb55464b7
Yessi Munguengui <yessimunguengui@gmail.com>
2022-03-25 16:58:33 +0100

    fix : filter & sort

--- artwork_gallery/static/js/filterArtworks.js
+++
@@ -1,8 +0,0 @@
-const updateURL = () => {
-    const selectElement = document.getElementById('filter');
-    return selectElement.addEventListener('change', (event) => {
-        window.location.href = `/artworks?category=${event.target.value}`
-    })
-}
-
-updateURL()
