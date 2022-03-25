19809d9ee75fbe3d08fe201b823c18a7e305878a
Auriane Labille <45030838+Fanghienne@users.noreply.github.com>
2022-03-25 11:52:16 +0100

    Update filterArtworks.js

--- artwork_gallery/static/js/filterArtworks.js
+++ artwork_gallery/static/js/filterArtworks.js
@@ -1,7 +1,6 @@
 const updateURL = () => {
     const selectElement = document.getElementById('filter');
     return selectElement.addEventListener('change', (event) => {
-        console.log(event.target.value)
         window.location.href = `/artworks?category=${event.target.value}`
     })
 }
