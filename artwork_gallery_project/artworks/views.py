7d77a6106324381b4ac7c42cecfcc52702f29d1f
Yessi Munguengui <yessimunguengui@gmail.com>
2022-03-31 16:34:43 +0200

    fix: add status code

--- artwork_gallery_project/artworks/views.py
+++ artwork_gallery_project/artworks/views.py
@@ -62,12 +62,12 @@ def artwork_detail(request, id):
 
 
 def error_403(request, exception):
-    return render(request, "artworks/error.html")
+    return render(request, "artworks/error.html", status=403)
 
 
 def error_404(request, exception):
-    return render(request, "artworks/404.html")
+    return render(request, "artworks/404.html", status=404)
 
 
 def error_500(request):
-    return render(request, "artworks/error.html")
+    return render(request, "artworks/error.html", status=500)
