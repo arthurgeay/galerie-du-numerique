73983fe77149970bab6032d8f4e30ebe9ba502fd
Yessi Munguengui <36877855+Yxssi@users.noreply.github.com>
2022-03-31 11:54:05 +0200

    Filtering artworks (#78)
    * UPD gallery
    
    * GET categories list
    
    * ADD filter artworks
    
    * Update index.html
    
    * fix : remove unsued method + order by id
    
    * fix: remove debug toolbar
    
    * ADD icons
    
    * Update gallery.css
    
    * Update filterArtworks.js
    
    * ADD artworks sorting
    
    * fix : filter & sort
    
    * fix : lint files
    
    * fix: add static_root
    
    * fix: static_root value
    
    * fix: syntax
    
    * review
    
    Co-authored-by: Auriane Labille <45030838+Fanghienne@users.noreply.github.com>
    Co-authored-by: Arthur Geay <arthurgeay.contact@gmail.com>

--- artwork_gallery_project/artworks/views.py
+++ artwork_gallery_project/artworks/views.py
@@ -2,20 +2,52 @@ from artworks.models import Artwork, Artist, Category
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-
+from django.db.models import Count
 from .models import Artwork
 
 
 @login_required()
 def gallery(request):
-    # TODO list by popularity
-    artwork_list = Artwork.objects.all()
+    filter_category = request.GET.get("category")
+    sort_artworks = request.GET.get("sort")
+
+    artwork_list = (
+        Artwork.objects.annotate(votes_count=Count("votes"))
+        .order_by("-votes_count")
+        .all()
+    )
+
+    categories = Category.objects.order_by("id").all()
+
+    if sort_artworks and sort_artworks == "ASC":
+        artwork_list = (
+            Artwork.objects.annotate(votes_count=Count("votes"))
+            .order_by("votes_count")
+            .all()
+        )
+    if filter_category:
+        if sort_artworks and sort_artworks == "ASC":
+            artwork_list = (
+                artwork_list.filter(category=filter_category)
+                .annotate(votes_count=Count("votes"))
+                .order_by("votes_count")
+                .all()
+            )
+        else:
+            artwork_list = (
+                artwork_list.filter(category=filter_category)
+                .annotate(votes_count=Count("votes"))
+                .order_by("-votes_count")
+                .all()
+            )
 
     paginator = Paginator(artwork_list, 4)  # Show 4 artworks per page
     page = request.GET.get("page")
     artworks = paginator.get_page(page)
 
-    return render(request, "artworks/index.html", {"artworks": artworks})
+    return render(
+        request, "artworks/index.html", {"artworks": artworks, "categories": categories}
+    )
 
 
 @login_required()
