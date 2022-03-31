8110bfc65251fc9172e096d5ede9d6964b4d98fb
Yessi Munguengui <yessimunguengui@gmail.com>
2022-03-31 09:31:07 +0200

    fix: add static_root

--- artwork_gallery_project/artwork_gallery/settings.py
+++ artwork_gallery_project/artwork_gallery/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATIC_ROOT = STATICFILES_DIRS
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
