a3a84fdbb165211b890a6f59aec7e250b60bd1b9
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-03-31 10:29:23 +0200

    feat: disable admin

--- artwork_gallery_project/artwork_gallery/urls.py
+++ artwork_gallery_project/artwork_gallery/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # TODO: Deactivate admin django
-    #path("admin-django/", admin.site.urls),
     path("", include("authentication.urls")),
     path("", include("presentation.urls")),
     path("artworks/", include("artworks.urls")),
